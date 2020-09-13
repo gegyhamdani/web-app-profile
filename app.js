f5b7a04eaa914ea306825ec37bbec9a8c3f3b047
gegyhamdani <gegyhamdani@gmail.com>
2020-09-14 00:16:37 +0700

    create notification section

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// Make window to top when open/refresh web
+window.scrollTo({ top: 0, behavior: "smooth" });
+
+// Get notification section
+var notification = document.getElementById("notification");
+
+// Get content body section
+var contentBody = document.getElementById("content-body");
+
+// Get the button that close the notification
+var notificationButton = document.getElementById("notification-button");
+
+// Function that hide the notification
+const handleHideNotification = () => {
+  notification.classList.add("notification-inActive");
+  contentBody.classList.add("content-body-full");
+};
+
+// Event to call hide notification function
+notificationButton.addEventListener("click", handleHideNotification);
+
+// Function to get year dynamically for footer
 document
   .getElementById("copyright")
   .appendChild(document.createTextNode(new Date().getFullYear()));
